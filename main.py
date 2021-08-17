651d3f878d7cb138e85427a4bae3c8c2315566c1
Jumpei-alumni <fujita.jumpei.sw@alumni.tsukuba.ac.jp>
2021-08-17 10:08:40 +0000

    first commit

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import streamlit as st 
-import numpy as np
-import pandas as pd 
 from PIL import Image
+import time
+
 
 st.title("streamlit 入門")
 
-st.write("data frame")
+"start!!"
 
-df = pd.DataFrame(data=[[i for i in range(4)],[i*10 for i in range(4)]])
-st.write(df)
-st.dataframe(df.style.highlight_max(axis=0))
-st.table(df.style.highlight_max(axis=0))
+latest_iteration = st.empty()
+bar = st.progress(0)
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+"Done"
 
-"""
-# 章
-## 節
-### 項
-```
+left_column, right_column = st.columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラムです')
 
-```python
-import streamlit as st 
-import numpy as np
-import pandas as pd 
+expander = st.expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
 
-```
-"""
 
-oresen_df = pd.DataFrame(data=np.random.rand(20, 3), columns=["a", "b", "c"])
-st.line_chart(oresen_df)
+option = st.sidebar.slider('your condition', 0, 100, 50)
+options = st.sidebar.text_input("what is your hobby?")
+st.write('your hobby is ',options)
+st.write("your condition : ", option)
 
-area_df = pd.DataFrame(data=[35.69, 139.70] + np.random.rand(100, 2) / [50, 50], columns=["lat", "lon"])
-st.map(area_df)
+option = st.selectbox(
+    'choose the number',
+    options=list(range(1, 11)))
 
-st.write("display image")
-image = Image.open("image.png")
-st.image(image, caption="numpy", use_column_width=True)
+'the chosen num : ', option
+if st.checkbox('show image'):
+
+    st.write("display image")
+    image = Image.open("image.png")
+    st.image(image, caption="numpy", use_column_width=True)
 
